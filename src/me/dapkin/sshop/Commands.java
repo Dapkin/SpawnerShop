f5e17e12780e854dd27dd5d8b6f8003152da90bd
Dapkin <dapkin10092@hotmail.com>
2015-06-11 14:38:30 -0500

    Fixed cooldown issue after creating new commands class.

--- src/me/dapkin/sshop/Commands.java
+++ src/me/dapkin/sshop/Commands.java
@@ -3,18 +3,15 @@ package me.dapkin.sshop;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
 
 public class Commands implements CommandExecutor {
 	
 	public ShopPlugin plugin;
-	final FileConfiguration config;
 	
 	public Commands(ShopPlugin plugin) {
 		this.plugin = plugin;
-		this.config = plugin.getConfig();
 	}
 	
 	@Override
@@ -24,28 +21,29 @@ public class Commands implements CommandExecutor {
 			if(args.length == 0) {
 				if(p.hasPermission("spawnershop.use")) {
 					if(plugin.cooldown.containsKey(p.getName())) {
-						int cooldown_time = config.getInt("options.cooldown");
-						long diff = (System.currentTimeMillis() - plugin.cooldown.get(p.getName()) / 1000);
+						int cooldown_time = plugin.config.getInt("options.cooldown");
+						long diff = (System.currentTimeMillis() - ((Long)plugin.cooldown.get(p.getName())).longValue()) / 1000L;
 						if(diff < cooldown_time) {
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', config.getString("options.cooldownmessage")));
+							p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', plugin.config.getString("options.cooldownmessage")));
 						}else {
 							p.openInventory(plugin.spawnerInv);
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', config.getString("options.openmessage")));
+							p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', plugin.config.getString("options.openmessage")));
+							plugin.cooldown.remove(p.getName());
 						}
 					}else {
 						p.openInventory(plugin.spawnerInv);
-			            p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', config.getString("options.openmessage")));
+			            p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', plugin.config.getString("options.openmessage")));
 					}
 				}else {
-					p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', config.getString("options.nopermission")));
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', plugin.config.getString("options.nopermission")));
 				}
 			}else if(args.length == 1 && args[0].equalsIgnoreCase("reload")) {
 				if(p.hasPermission("spawnershop.reload") || p.isOp()) {
 					plugin.spawnerInv.clear();
 					plugin.setupInv();
-					p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', config.getString("options.reloadsuccess")));
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', plugin.config.getString("options.reloadsuccess")));
 				}else {
-					p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', config.getString("options.nopermission")));
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', plugin.config.getString("options.nopermission")));
 				}
 			}
 		}
